4fa8168a3e6fa67858fb603d81ea45054959b83d
martapa <martabioinf@gmail.com>
2022-09-08 21:20:15 -0400

    chore: change file names

--- src/stories/primitives/Input.stories.jsx
+++ src/stories/primitives/Input.stories.jsx
@@ -3,7 +3,7 @@ import Input from './Input';
 import { blockProp } from '@crcls/blocksui-sdk';
 
 export default {
-  title: 'Base Blocks/Input',
+  title: 'Primitives/Input',
   component: Input,
 };
 
